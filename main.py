a898228125cc5947858e59fed891ff6f274a7ace
Nix embrace <71151068+Estex-cyber@users.noreply.github.com>
2021-03-12 23:19:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import psutil
+
+battery = psutil.sensors_battery()
+
+if battery.power_plugged:
+    print("Charging", battery.percent, "%")
+
+elif not battery.power_plugged:
+    print("Not charging", battery.percent, "%")
+    print("Discharge time", int(battery.secsleft), "sec left")
+
+if int(battery.percent <= 10):
+    if bool(battery.power_plugged == False):
+        print("Can't be played")
+        if int(battery.percent == 10) or int(battery.percent < 10):
+            print("**Warning**", battery.percent, "%", "Low")
+            if int(battery.percent == 5) or int(battery.percent < 5):
+                print("Very low!")
+                quit()
+    elif bool(battery.power_plugged == True):
+        print("Continue charging")
