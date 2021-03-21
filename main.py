2a38b5fe380a75fc208d2b53e848f52dfca46012
Nix embrace <71151068+Estex-cyber@users.noreply.github.com>
2021-03-21 23:32:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ elif not battery.power_plugged:
     print("Not charging", battery.percent, "%")
     print("Discharge time", int(battery.secsleft), "sec left")
 
-if int(battery.percent <= 10):
-    if bool(battery.power_plugged == False):
-        print("Can't be played")
-        if int(battery.percent == 10) or int(battery.percent < 10):
-            print("**Warning**", battery.percent, "%", "Low")
-            if int(battery.percent == 7) or int(battery.percent < 7):
-                print("Very low!")
-                print("Needs to be charged RightNow")
-                quit()
+if int(battery.percent == 10) or int(battery.percent < 10):
+    if bool(battery.power_plugged == False)
+        print("battery is low!", battery.percent, "%)
+        if int(battery.percent == 7) or int(battery.percent < 7):
+            print("**Warning**", battery.percent, "battery is very low!", "%")
+            quit()
     elif bool(battery.power_plugged == True):
         print("Continue charging")
